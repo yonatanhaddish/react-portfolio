56ba2767ac981cbbb3e1d07fae3b86c1da1d1a3c
Your Name <you@example.com>
2021-07-10 11:36:58 -0400

    page template

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [categories]= useState([
+    { name: "About me", description: "about me" },
+    { name: "Portfolio", description: "my portfolio"},
+    // { name: "Contact", description: "contact me"},
+    { name: "Resume", description: "my resume"}
+  ]);
+  const [currentCategory, setCurrentCategory]= useState(categories(0));
+  return (
+    <div>
+      <Nav categories= {categories}
+          currentCategory= {currentCategory}
+      ></Nav>
+    </div>
+  );
+}
+
+export default App;
