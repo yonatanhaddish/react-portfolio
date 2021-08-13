a02a8799dd9781ff0a647824bbaa6584959b8886
Your Name <you@example.com>
2021-08-13 14:18:53 -0400

    styling some components

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,14 +11,12 @@ function AboutMe() {
                 alt="profile"
                 className="img-thumbnail mx-1"
                 key="profile" />
-            <p className="about-me">This is where information of me goes.
-            This is where information of me goes.
-            This is where information of me goes.
-            This is where information of me goes.
-            This is where information of me goes.
-            This is where information of me goes.
-            This is where information of me goes.
-             </p>
+            <p className="about-me">
+                Hi! I'm Yonatan. I am a full stack web developer who recently graduated from University of Toronto.
+                I have a BSc in Applied Mathematics. I am also a self taught in the basic of Python programming language.
+                I like working with a team members because I believe we learn more from each other. When I am working with
+                team members or clients, my goal is to provide the best service, and user-friendly high level product.
+            </p>
         </section>
     )
 }
