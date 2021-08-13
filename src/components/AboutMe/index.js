338d15f6ce12031509f0154504e2a8c30b792836
Your Name <you@example.com>
2021-08-13 14:46:34 -0400

    deploying into website

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -13,9 +13,10 @@ function AboutMe() {
                 key="profile" />
             <p className="about-me">
                 Hi! I'm Yonatan. I am a full stack web developer who recently graduated from University of Toronto.
-                I have a BSc in Applied Mathematics. I am also a self taught in the basic of Python programming language.
+                I have also BSc in Applied Mathematics. I am also a self taught in basic of Python programming language.
                 I like working with a team members because I believe we learn more from each other. When I am working with
                 team members or clients, my goal is to provide the best service, and user-friendly high level product.
+                
             </p>
         </section>
     )
