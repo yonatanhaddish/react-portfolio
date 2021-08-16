faf72e8074dc660636a72a8d9cc535089e3213b9
Yonatan Haddish <yonihailshn@gmail.com>
2021-08-16 16:45:41 -0400

    resume added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ function Footer() {
     return(
         <section className="footer">
             <div className="">
-                <a href="https://github.com/yonatanhaddish">Github</a>
+                <a href="https://github.com/yonatanhaddish" target="_blank" rel="noopener noreferrer">Github</a>
             </div>
             <div>
-            <a href="https://www.linkedin.com/in/yonatan-haddish-2a5b93205/">Linkedin</a>
+            <a href="https://www.linkedin.com/in/yonatan-haddish-2a5b93205/" target="_blank" rel="noopener noreferrer">Linkedin</a>
             </div>
         </section>
     )
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,29 +5,30 @@ function Resume() {
         <section>
             <h2 className="h2-title">Resume</h2>
             <div>
-                <h3>Front-End</h3>
+                <h3>Tools</h3>
                 <ol>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>JQuery</li>
-                    <li>React</li>
-                    <li>Bootstrap</li>
+                    <li className="tool-list">HTML</li>
+                    <li className="tool-list">CSS</li>
+                    <li className="tool-list">JavaScript</li>
+                    <li className="tool-list">JQuery</li>
+                    <li className="tool-list">React</li>
+                    <li className="tool-list">Bootstrap</li>
+                    <li className="tool-list">Styled-components</li>
+                    <li className="tool-list">APIs</li>
+                    <li className="tool-list">Node</li>
+                    <li className="tool-list">Express</li>
+                    <li className="tool-list">MySQL, Sequelize</li>
+                    <li className="tool-list">REST</li>
+                    <li className="tool-list">GraphQL</li>
+                    <li className="tool-list">MongoDb, Graphql</li>
                 </ol>
             </div>
             <div>
-            <h3>Back-End</h3>
-                <ol>
-                    <li>APIs</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MySQL, Sequelize</li>
-                    <li>REST</li>
-                    <li>GraphQL</li>
-                </ol>
+                <a href="https://drive.google.com/file/d/1Nt_UonDxXlBgO29XMTa4dcOOS7pG_q3R/view?usp=sharing"
+                    target="_blank" rel="noopener noreferrer" >
+                    <button>Download Resume</button>
+                </a>
             </div>
-            
-
         </section>
     );
 }
