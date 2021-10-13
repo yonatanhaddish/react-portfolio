779e8b9b897fa4da21845a3d274669f4784b2551
Yonatan Haddish <yonihailshn@gmail.com>
2021-10-13 10:59:26 -0400

    About section some adjustments

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -12,11 +12,13 @@ function AboutMe() {
                 className="img-thumbnail mx-1"
                 key="profile" />
             <p className="about-me">
-                Hi! I'm Yonatan. I am a full stack web developer who recently graduated from University of Toronto.
-                I have also BSc in Applied Mathematics. I am also a self taught in basic of Python programming language.
-                I like working with a team members because I believe we learn more from each other. When I am working with
-                team members or clients, my goal is to provide the best service, and user-friendly high level product.
-                
+            Hi! I’m Yonatan. I am a full stack web developer who recently earned a certificate in Web Development
+            from University of Toronto, with newly developed skills in JavaScript, CSS, React.js, and responsive
+            web design. I also have BSc in Applied Mathematics. I am also a self-taught in basic of Python 
+            programming language. I like working with a team member because I believe we learn more from each 
+            other. When I am working with team members or clients, my goal is to provide the best service, and 
+            user-friendly high-level product. I’m excited to leverage my skills as part of a fast-paced, 
+            quality-driven to build better experience on the web.
             </p>
         </section>
     )
