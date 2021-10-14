34fa5b796fe0decdb40afdba608be02e078a1ae5
Yonatan Haddish <yonihailshn@gmail.com>
2021-10-14 16:19:31 -0400

    some adjustments in portfolio section

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,7 +10,7 @@ function Portfolio(proppass) {
                 {/* <div className="innercard"> */}
                     {projects.map((project) => (
                         <div className="newcard" key={project.name}>
-                            <div style={{backgroundImage: `url(./assets/bk_img/Food Festival.png)`}} className="background-card flex-child">
+                            <div style={{backgroundImage: `url()`}} className="background-card flex-child">
                                 <h1>{project.name}</h1>
 
                             </div>
